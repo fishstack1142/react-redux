ba0adc3fe9986762afb1d883d7dad21a81f7969b
fishstack1142 <fishstack1142@gmail.com>
2019-09-03 06:07:41 +0700

    add reducer to store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import HelloWorld from './HelloWorld';
+import reducer from "./reducers";
+import { createStore } from "redux";
+
+const initialState = { tech: "React-Init", techies: "techies" };
+const store = createStore(reducer, initialState);
 
 
 
 class App extends Component {
 
-  state = {
-    tech: "React-Redux"
-  };
+  // state = {
+  //   tech: "React-Redux"
+  // };
 
   render () {
 
-    return <HelloWorld tech={this.state.tech} /> 
+    // return <HelloWorld tech={this.state.tech} /> 
+    return <HelloWorld tech={store.getState().techies} /> 
   };
 }
 
